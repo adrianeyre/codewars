60ab9e2674ae3a94d320f3b4f8cd3b37d02a16e5
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-12-06 21:06:11 +0000

    Added BoolYN.rb

---
+++ Ruby/8KYU/BoolYN.rb
@@ -0,0 +1,14 @@
+=begin
+
+Complete the bool_to_word (Javascript: boolToWord ) method.
+
+Given: a boolean value
+
+Return: a 'Yes' string for true and a 'No' string for false
+
+=end
+
+# My Solution
+def bool_to_word bool
+  bool ? 'Yes' : 'No'
+end
