da72eb3548e092b19fcfbc3a5b5dd900a96fa188
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-12-06 20:52:04 +0000

    Added MakeChange.rb

---
+++ Ruby/8KYU/MakeChange.rb
@@ -0,0 +1,26 @@
+=begin
+
+You go to the store and have a 10 dollar bill to spend.
+You buy candy, chips, and soda. Find out how much change
+you get back from the cashier.
+
+Item	Cost
+Candy	$1.42
+Chips	$2.40
+Soda	$1.00
+Create 5 variables and use the cost from the table above to set their values.
+
+money
+candy
+chips
+soda
+change
+
+=end
+
+# My solution
+money = 10
+candy = 1.42
+chips = 2.4
+soda = 1
+change = money - candy - chips - soda
