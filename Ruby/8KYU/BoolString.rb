c0b2a7d36e8388dd17c94232bece05e663387bde
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-12-06 21:02:48 +0000

    Added BoolString.rb

---
+++ Ruby/8KYU/BoolString.rb
@@ -0,0 +1,36 @@
+=begin
+n this programming exercise, you're going to learn about functions,
+boolean (true/false) values, strings, and the if-statement.
+
+A function is a block of code that takes an input and produces an output.
+In this example, boolean_to_string is a function whose input is
+either true or false, and whose output is the string representation of
+the input, either "true" or "false".
+
+A common idea we often want to represent in code is the concept
+of true and false. A variable that can either be true or false
+is called a boolean variable. In this example, the input to
+boolean_to_string (represented by the variable b) is a boolean.
+
+Lastly, when we want to take one action if a boolean is true,
+and another if it is false, we use an if-statement.
+
+For this kata, don't worry about edge cases like where unexpected
+input is passed to the function. You'll get to worry about these
+enough in later exercises.
+
+=end
+
+# My solution
+def boolean_to_string(b)
+  if b
+    "true"
+  else
+    "false"
+  end
+end
+
+# Better solution
+def boolean_to_string(b)
+  b ? "true" : "false"
+end
