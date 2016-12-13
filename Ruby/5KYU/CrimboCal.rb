a4b315a3a30eb7d148ed935d545d2763a8dc3036
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-12-13 13:20:34 +0000

    CrimboCal.rb Added

---
+++ Ruby/5KYU/CrimboCal.rb
@@ -0,0 +1,20 @@
+=begin
+
+It's almost Christmas and you have a pretty cool Christmas Calendar with tasty
+sweets and awesome toys in it. As curious as you are, you want to open all the
+doors. But unfortunately only the today's door can be opened. Can you find a
+way to open the 24th door already? :)
+
+PS: Yes, if you do this kata on December 24th, you'll get a solved kata for
+free. But the other 364 days of the year, you have to trick a bit.
+
+Good Luck and Merry X-Mas!
+
+=end
+
+# My Solution
+class Date
+  def Date.today
+    return Date.new(2016,12,24)
+  end
+end
