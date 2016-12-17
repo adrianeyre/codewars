76593de733ebc7039f41c73fdaeab3bbf80a6d37
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-12-17 18:13:15 +0000

    Added files

---
+++ Ruby/7KYU/ReverseString.rb
@@ -0,0 +1,12 @@
+=begin
+
+Complete the solution so that it reverses the string value passed into it.
+
+solution('world') # returns 'dlrow'
+
+=end
+
+# My Solution
+def solution(str)
+  str.reverse
+end
