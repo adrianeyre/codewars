63ad7e108e462ee3519937ff5ebe2ffc057067ef
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-19 19:44:20 +0000

    file update

--- Ruby/Authored/Mastermind.rb
+++ Ruby/Authored/Mastermind.rb
@@ -1,18 +1,28 @@
-def mastermind(game)
-  colours = ["Red", "Blue", "Green", "Orange", "Purple", "Yellow"]
-  amount = Hash.new {|key,value| key[value]=0}
-  guess = []
-  colours.each do |colour|
-    answer = game.check([colour, colour, colour, colour])
-    colour_amount = answer.count("Black")
-    amount[colour] = colour_amount
-    (1..colour_amount).each{|c| guess << colour}
-  end
-  answer = false
+# srand(1)
+# def mastermind(game)
+#   colours = ["Red", "Blue", "Green", "Orange", "Purple", "Yellow"]
+#   amount = Hash.new {|key,value| key[value]=0}
+#   guess = []
+#   colours.each do |colour|
+#     answer = game.check([colour, colour, colour, colour])
+#     colour_amount = answer.count("Black")
+#     amount[colour] = colour_amount
+#     (1..colour_amount).each{|c| guess << colour}
+#   end
+#   answer = false
+#
+#   while answer != "WON!" && answer != "Error"
+#     p answer = game.check(guess.shuffle)
+#   end
+# end
 
-  while answer != "WON!" && answer != "Error"
-    p answer = game.check(guess.shuffle)
-  end
+srand(1)
+
+
+def mastermind(game)
+  # Your code here
+  answer = game.check(["Yellow", "Orange", "Purple", "Red"])
+  p answer
 end
 
 class MasterMind
@@ -20,6 +30,7 @@ class MasterMind
   Colours = ["Red", "Blue", "Green", "Orange", "Purple", "Yellow"]
 
   def initialize()
+    srand
     @result = []
     @goes = 0
     (1..4).each do |colour|
@@ -32,9 +43,10 @@ class MasterMind
   def check(attempt)
     return "Error" if attempt.length != 4
     @goes += 1
-    p @goes
     return "Error" if @goes>60
-    return "WON!" if attempt == @result
+    if attempt == @result
+      return "WON!"
+    end
     amount = {"Red"=>0, "Blue"=>0, "Green"=>0, "Orange"=>0, "Purple"=>0, "Yellow"=>0}
     @amount.each{|key,value| amount[key]=value}
     result = []
