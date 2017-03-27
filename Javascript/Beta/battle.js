713421a7406db794779819d7da2d46aa168331c0
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-03-27 13:55:16 +0100

    Added file

--- Javascript/Beta/battle.js
+++ Javascript/Beta/battle.js
@@ -2,21 +2,38 @@ function damagedOrSunk (board, attacks){
   // console.log(board);
   // console.log(attacks);
   // console.log(board[attacks[x][0]][attacks[x][1]]);
+  var sunk = 0;
+  var damaged = 0;
+  var notTouched = 0;
+  var points = 0;
+  var hits = {1:0, 2:0, 3:0, 4:0, 5:0};
+
   for(var x=0;x < attacks.length;x++){
-    a = attacks[x][0];
-    b = attacks[x][1];
-    pos = board[a-1][b-1]
-    console.log(board[a-1][b-1]);
+    // a = attacks[x][0];
+    // b = attacks[x][1];
+    pos = board[attacks[x][0]-1][attacks[x][1]-1];
+    // console.log(board[a-1][b-1]);
+    // hits[pos] ++;
+    if (pos > 0){sunk++; hits[pos] ++;}
   }
+  console.log(hits);
+  points = (sunk * 1);
+  return {sunk: sunk,damaged: damaged,notTouched: notTouched,points: points};
 }
 
 
-b = [ [ 0, 0, 1, 2, 2, 0 ],
- [ 0, 3, 0, 1, 0, 0 ],
-  [ 0, 3, 0, 0, 0, 0 ],
-  [ 0, 3, 0, 0, 0, 0 ] ];
+board = [[3, 0, 1],
+         [3, 0, 1],
+         [0, 2, 1],
+         [0, 2, 0]];
 
-a =  [ [ 3, 4 ], [ 4, 3 ], [ 4, 4 ] ];
-console.log(damagedOrSunk(b,a));
+attacks = [[2, 1], [2, 2], [3, 2]];
+
+result = damagedOrSunk(board, attacks);
+console.log(result);
+console.log(result.sunk);
+console.log(result.damaged);
+console.log(result.notTouched);
+console.log(result.points);
 
 // { sunk: 1, damaged: 1 , notTouched: 1, points: 0.5}
--- Swift/Authored/drive.swift
+++ Swift/Authored/drive.swift
@@ -17,3 +17,43 @@ func drive(_ drinks: [[Double]], _ finished: String, _ drive_time: String) -> [A
 
     return [round(100 * total_units) / 100 , time_when_can_drive < ddtt]
 }
+
+// alcohol = [[5.2,568],[12.0,175]];
+// Test.assertDeepEquals(drive(alcohol, "16:00", "23:00"), [5.05, true], "Expected '[5.05, true]'");
+//
+// alcohol = [[5.2,568],[5.2,568],[5.2,568],[12.0,175],[12.0,175],[12.0,175]];
+// Test.assertDeepEquals(drive(alcohol, "23:00", "08:15"), [15.16, false], "Expected '[15.16, false]'")
+//
+// alcohol = [[15.5,568]];
+// Test.assertDeepEquals(drive(alcohol, "23:00", "06:45"), [8.8, false], "Expected '[8.8, false]'");
+//
+// alcohol = [[10.0,100]];
+// Test.assertDeepEquals(drive(alcohol, "20:00", "21:00"), [1.0, false], "Expected '[1.0, false]'");
+//
+// alcohol = [[10.0,100]];
+// Test.assertDeepEquals(drive(alcohol, "20:00", "21:01"), [1.0, true], "Expected '[1.0, true]'");
+//
+// function drive_tester(drinks2, finished2, drive_time2) {
+//   total_units2 = 0;
+//   for(var gg=0; gg < drinks2.length;gg++){
+//     total_units2 += drinks2[gg][0].toPrecision() * drinks2[gg][1] / 1000;
+//   }
+//   fda2 = finished2.split(":");
+//   finished2 = new Date(1990,00,01,fda2[0],fda2[1]);
+//   dtt2 = drive_time2.split(":");
+//   drive_time2 = new Date(1990,00,01,dtt2[0],dtt2[1]);
+//   if (finished2 > drive_time2){drive_time2.setDate(drive_time2.getDate() + 1);}
+//   var time_when_can_drive2 = new Date(finished2.getTime() + (total_units2*60*60*1000));
+//   return [Number(total_units2.toFixed(2)),time_when_can_drive2 < drive_time2];
+// }
+//
+// for (testsi = 1; testsi <= 250; testsi++) {
+//   var drinks_li = [];
+//   for (dr = 1; dr <= Math.floor(Math.random() * 10)+1; dr++) {
+//     drinks_li.push([parseFloat((Math.random() * 20).toFixed(2)), Math.floor(Math.random() * 500)+75]);
+//   }
+//   ft = String(Math.floor(Math.random() * 12)+10) + ":" + String(Math.floor(Math.random() * 49)+10);
+//   dt = String(Math.floor(Math.random() * 12)+10) + ":" + String(Math.floor(Math.random() * 49)+10);
+//   solution = drive_tester(drinks_li, ft, dt);
+//   Test.assertDeepEquals(drive(drinks_li, ft, dt), solution, "Expected '"+solution+"'");
+// }
